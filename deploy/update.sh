c0987e81133863d06dfd6b077bbd51d98d9b7bb1
mmillares <manuelmillares1@gmail.com>
2020-08-30 17:22:36 -0400

    Changed configuration for deployment

--- deploy/update.sh
+++ deploy/update.sh
@@ -7,6 +7,6 @@ PROJECT_BASE_PATH='/usr/local/apps/ecommerce-rest-api'
 git pull
 $PROJECT_BASE_PATH/env/bin/python manage.py migrate
 $PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
-supervisorctl restart profiles_api
+supervisorctl restart ecommerce_api
 
 echo "DONE! :)"
